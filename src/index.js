3917c5d21f40872738070c27df440ab941a7ba2b
veronikabassak <106739795+veronikabassak@users.noreply.github.com>
2022-08-18 10:53:06 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -42,16 +42,20 @@ function convertToCelsius(event) {
   temperatureElement.innerHTML = 19;
 }
 
-// Feature #2
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
 
-// Bonus Feature
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", convertToCelsius);
+
+function getForecast(coordinates) {
+  let apiKey = "6c216998e3e1c3107c772781f84e2468";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -73,11 +77,12 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
 }
 
 function search(city) {
   let apiKey = "6c216998e3e1c3107c772781f84e2468";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
